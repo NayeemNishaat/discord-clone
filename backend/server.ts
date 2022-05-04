69c38368322f47e4e4228d368ce5c09c71cb80f7
NayeemNishaat <rxNayeemIMI@gmail.com>
2022-05-05 01:24:51 +0600

    created server config

--- backend/server.ts
+++ backend/server.ts
@@ -8,5 +8,13 @@ const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT: String = process.env.PORT || process.env.API_PORT;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+try {
+	const connection = await mongoose.connect(process.env.MONGO_URI);
+
+	if (!connection) throw new Error("Failed to connect with DB and Server!");
+
+	const PORT: String = process.env.PORT || process.env.API_PORT;
+	app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+} catch (err) {
+	console.log(err);
+}
