69e06b235599b8a5ce287345615d6a8322a6e884
NayeemNishaat <rxNayeemIMI@gmail.com>
2022-05-09 00:22:38 +0600

    implemented redux with rtk

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from "next";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../redux/store";
+import { test } from "../redux/slices/authSlice";
 
-const Home: NextPage = () => {
-    return <section className="text-5xl font-bold">Welcome!</section>;
+const Index: NextPage = () => {
+	const id = useSelector((state: RootState) => state.auth.id);
+
+	const dispatch = useDispatch();
+
+	return (
+		<section className="text-5xl font-bold">
+			<p>{id}</p>
+			<button
+				aria-label="Increment value"
+				onClick={() => dispatch(test("Nayeem"))}
+			>
+				Set ID
+			</button>
+		</section>
+	);
 };
 
-export default Home;
+export default Index;
