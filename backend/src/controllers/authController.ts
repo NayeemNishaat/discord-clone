47c88fd8fb1b73da221995a00ccff012cdda0ea8
NayeemNishaat <rxNayeemIMI@gmail.com>
2022-05-08 16:40:41 +0600

    implemented route protection

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -4,6 +4,13 @@ import jwt from "jsonwebtoken";
 import { catchAsync, AppError } from "../lib/error";
 import User from "../models/userModel";
 
+interface customRequest extends Request {
+	user: {
+		_id: string;
+		username: string;
+		email: string;
+	};
+}
 interface userSchema {
 	_id: string;
 	username: string;
@@ -12,6 +19,11 @@ interface userSchema {
 	email: string;
 	token: string;
 }
+interface JwtPayload {
+	id: string;
+	email: string;
+}
+
 declare const process: {
 	env: {
 		JWT_SECRET: string;
@@ -21,21 +33,29 @@ declare const process: {
 };
 
 const createSendToken = (
-	info: { id: string; email: string },
+	user: userSchema,
+	statusCode: number,
 	req: Request,
 	res: Response
 ) => {
-	const token = jwt.sign(info, process.env.JWT_SECRET, {
-		expiresIn: process.env.JWT_EXPIRES_IN
-	});
+	const token = jwt.sign(
+		{ id: user._id, email: user.email },
+		process.env.JWT_SECRET,
+		{
+			expiresIn: process.env.JWT_EXPIRES_IN
+		}
+	);
 
 	const cookieOptions = {
 		expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 1), // Note: Must be number!
 		httpOnly: true,
-		secure: true
+		secure: req.secure // Important: req.secure is false in localhost. If it is true it won't be available via document.cookie but in http header!
 	};
 
 	res.cookie("jwt", token, cookieOptions);
+	user.password = undefined;
+
+	res.status(statusCode).json({ status: "success", data: user });
 };
 
 export const register = catchAsync(
@@ -54,11 +74,7 @@ export const register = catchAsync(
 			confirmPassword: req.body.confirmPassword
 		});
 
-		createSendToken({ id: newUser._id, email: newUser.email }, req, res);
-
-		newUser.password = undefined;
-
-		res.status(201).json({ status: "success", data: newUser });
+		createSendToken(newUser, 201, req, res);
 	}
 );
 
@@ -77,10 +93,47 @@ export const login = catchAsync(
 			return next(new AppError("Invalid credentials!", 400));
 		}
 
-		createSendToken({ id: user._id, email: user.email }, req, res);
+		createSendToken(user, 200, req, res);
+	}
+);
 
-		user.password = undefined;
+export const protect = catchAsync(
+	async (req: customRequest, res: Response, next: NextFunction) => {
+		let token;
+		if (req.headers.cookie) {
+			req.headers.cookie.split("; ").forEach((el) => {
+				const arr = el.split("=");
+				if (arr[0] === "jwt") token = arr[1];
+			});
+		}
 
-		res.status(200).json({ status: "success", data: user });
+		if (!token)
+			return next(new AppError("Please log in to get access!", 401));
+
+		const decoded = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload; // Warning: Overwriting the default JwtPayload interface with our JwtPayload interface!
+
+		const currentUser = await User.findById(decoded.id);
+
+		if (!currentUser)
+			return next(
+				new AppError(
+					"The user belonging to this token does no longer exist.",
+					401
+				)
+			);
+
+		// Point: Check if the user changed the password after the token was issued is not implemented because we are not providing the password changing functionality here!
+
+		req.user = currentUser;
+		next();
+	}
+);
+export const secret = catchAsync(
+	async (req: customRequest, res: Response, next: NextFunction) => {
+		res.status(200).json({
+			status: "success",
+			message: "You are accessing a protected route!",
+			user: req.user
+		});
 	}
 );
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,8 +1,14 @@
 import express, { Router } from "express";
 const router: Router = express.Router();
-import { register, login } from "../controllers/authController";
+import {
+	register,
+	login,
+	protect,
+	secret
+} from "../controllers/authController";
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/test", protect, secret);
 
 export default router;
