8c761aa32da161d5d1de2ace0f3cd02370f3104c
NayeemNishaat <rxNayeemIMI@gmail.com>
2022-05-16 00:26:46 +0600

    index page completed

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,49 +1,63 @@
 import type { NextPage } from "next";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
-import { useState, useEffect } from "react";
 import Button from "../components/UI/Button";
 import Link from "next/link";
+import { CircularProgress } from "@mui/material";
+import { useRouter } from "next/router";
 
 const Index: NextPage = () => {
-	// const [loginInfo, setLoginInfo] = useState<RootState["auth"]>();
+	const [component, setComponent] = useState(
+		<CircularProgress color="secondary" />
+	);
+
 	const loginInfo = useSelector((state: RootState) => state.auth);
+	const router = useRouter();
 
 	useEffect(() => {
-		// const storedLoginInfo = JSON.parse(
-		// 	localStorage.getItem("loginInfo") || "null"
-		// );
-		// if (!storedLoginInfo) return;
-		// setLoginInfo(useSelector((state: RootState) => state.auth));
-		// console.log(loginInfo);
-	}, []);
+		let timeout: NodeJS.Timeout;
+		if (loginInfo._id) router.push("/dashboard");
+		else {
+			timeout = setTimeout(
+				() =>
+					setComponent(
+						<div>
+							<p className="text-5xl font-bold">
+								Welcome to Discord Clone
+							</p>
+							<div className="mt-5 flex justify-center gap-5">
+								<Link href="/login">
+									<a>
+										<Button
+											label="Log In"
+											styles="text-white text-lg bg-black hover:bg-[#ff0038]"
+										/>
+									</a>
+								</Link>
 
-	// const loginInfo = useSelector((state: RootState) => state.auth);
+								<Link href="/register">
+									<a>
+										<Button
+											label="Register"
+											styles="text-black text-lg bg-white hover:bg-[#ff7b00]"
+										/>
+									</a>
+								</Link>
+							</div>
+						</div>
+					),
+				2500
+			);
+		}
+
+		() => clearTimeout(timeout);
+	}, [loginInfo]);
 
 	return (
 		<section className="w-full bg-[#5866f2]">
-			{loginInfo.username}
 			<div className="flex h-screen flex-col items-center justify-center ">
-				<p className="text-5xl font-bold">Welcome to Discord Clone</p>
-				<div className="mt-5 flex gap-5">
-					<Link href="/login">
-						<a>
-							<Button
-								label="Log In"
-								styles="text-white text-lg bg-black hover:bg-[#ff0038]"
-							/>
-						</a>
-					</Link>
-
-					<Link href="/register">
-						<a>
-							<Button
-								label="Register"
-								styles="text-black text-lg bg-white hover:bg-[#ff7b00]"
-							/>
-						</a>
-					</Link>
-				</div>
+				{component}
 			</div>
 		</section>
 	);
