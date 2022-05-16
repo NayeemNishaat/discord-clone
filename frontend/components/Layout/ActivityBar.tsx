38ae84baa19875da0c8c60d2b66dedd73bff3d60
NayeemNishaat <rxNayeemIMI@gmail.com>
2022-05-16 18:50:31 +0600

    Icons added in activity bar

--- frontend/components/Layout/ActivityBar.tsx
+++ frontend/components/Layout/ActivityBar.tsx
@@ -1,10 +1,12 @@
 import IconButton from "@mui/material/IconButton";
-// import DeleteIcon from "@mui/icons-material/Delete";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function ActivityBar() {
 	return (
 		<div className="flex h-screen w-20 flex-col items-center bg-[#202225]">
-			fggg
+			<IconButton aria-label="delete">
+				<DeleteIcon />
+			</IconButton>
 		</div>
 	);
 }
