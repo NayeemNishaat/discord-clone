c29461fbc785f953c9be5b6f48e7b55f57209aeb
NayeemNishaat <rxNayeemIMI@gmail.com>
2022-05-19 00:24:15 +0600

    working on logout

--- frontend/components/Invite/InviteItem.tsx
+++ frontend/components/Invite/InviteItem.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { Button } from "@mui/material";
+import CheckSharpIcon from "@mui/icons-material/CheckSharp";
+import ClearSharpIcon from "@mui/icons-material/ClearSharp";
+import IconButton from "@mui/material/IconButton";
 
 function InviteItem({
 	id,
 	username,
-	acceptInvitation, // Fix:
-	rejectInvitation // Fix:
+	acceptInvitation,
+	rejectInvitation
 }: {
 	id: string;
 	username: string;
@@ -15,16 +18,33 @@ function InviteItem({
 	const [disable, setDisable] = useState(false); // Fix:
 
 	return (
-		<Button className="flex items-center justify-between px-0 py-1.5 capitalize text-white">
-			{/* Warning: Should have created a component */}
+		<div className="flex items-center">
+			<Button className="flex w-full items-center justify-between px-0 py-1.5 capitalize text-white">
+				{/* Warning: Should have created a component */}
+				<div className="flex gap-2">
+					<span className="flex h-7 w-7 items-center justify-center rounded-full bg-[#2196f3] leading-none">
+						{username.slice(0, 1)}
+					</span>
+					<span>{username}</span>
+				</div>
+			</Button>
 			<div className="flex gap-2">
-				<span className="flex h-7 w-7 items-center justify-center rounded-full bg-[#2196f3] leading-none">
-					{username.slice(0, 1)}
-				</span>
-				<span>{username}</span>
+				<IconButton
+					className="h-6 w-6 border border-solid"
+					color="warning"
+					onClick={acceptInvitation}
+				>
+					<CheckSharpIcon />
+				</IconButton>
+				<IconButton
+					className="h-6 w-6 border border-solid"
+					color="warning"
+					onClick={rejectInvitation}
+				>
+					<ClearSharpIcon />
+				</IconButton>
 			</div>
-		</Button>
-		// Bug:
+		</div>
 	);
 }
 
