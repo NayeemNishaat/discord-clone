6e32aec79d4b40f69c62e980dd5dc5e06172675e
NayeemNishaat <rxnayeemimi@gmail.com>
2022-05-20 11:42:14 +0600

    working on sockerServer.ts

--- backend/src/socketServer.ts
+++ backend/src/socketServer.ts
@@ -1,8 +1,25 @@
 import { Server as socketServer, Socket } from "socket.io";
 import { Server as httpServer } from "http";
-import verifyJwt from "./lib/verifyJwt";
+import { AppError } from "./lib/error";
+import jwt from "jsonwebtoken";
+import User from "./models/userModel";
+
+interface JwtPayload {
+	id: string;
+	email: string;
+}
+
+declare const process: {
+	env: {
+		JWT_SECRET: string;
+		JWT_EXPIRES_IN: string;
+		JWT_COOKIE_EXPIRES_IN: number;
+	};
+};
 
 const configureSocketServer = (httpServer: httpServer) => {
+	let token: string;
+
 	const io = new socketServer(httpServer, {
 		cors: {
 			origin: "http://localhost:3000",
@@ -12,7 +29,29 @@ const configureSocketServer = (httpServer: httpServer) => {
 	});
 
 	io.use(async (socket, next) => {
-		verifyJwt(socket.handshake.headers.cookie, next);
+		if (socket.handshake.headers.cookie) {
+			socket.handshake.headers.cookie.split("; ").forEach((el) => {
+				const arr = el.split("=");
+				if (arr[0] === "jwt") token = arr[1];
+			});
+		}
+
+		if (!token)
+			return next(new AppError("Please log in to get access!", 401));
+
+		const decoded = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
+
+		const currentUser = await User.findById(decoded.id);
+
+		if (!currentUser)
+			return next(
+				new AppError(
+					"The user belonging to this token does no longer exist.",
+					401
+				)
+			);
+
+		socket.data = currentUser;
 		next();
 	});
 
