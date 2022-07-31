7617e9cdea12950e42791e1ad41e066991e8755f
nayeemnishaat <rxnayeemimi@gmail.com>
2022-07-31 21:52:29 +0600

    fixed local stream issue

--- frontend/components/Media/VideoItem.tsx
+++ frontend/components/Media/VideoItem.tsx
@@ -1,31 +1,31 @@
 import { useRef, useEffect } from "react";
 
 function MediaItem({
-	stream,
-	currentStream,
-	user
+  stream,
+  currentStream,
+  user
 }: {
-	stream: MediaStream;
-	currentStream: boolean;
-	user: {
-		_id: string | null;
-		username: string | null;
-	};
+  stream: MediaStream;
+  currentStream: boolean;
+  user: {
+    _id: string | null;
+    username: string | null;
+  };
 }) {
-	const videoRef = useRef<HTMLVideoElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
-	useEffect(() => {
-		videoRef.current && (videoRef.current.srcObject = stream);
-	}, []);
+  useEffect(() => {
+    videoRef.current && (videoRef.current.srcObject = stream);
+  }, [stream]);
 
-	return (
-		<video
-			muted={currentStream}
-			autoPlay
-			ref={videoRef}
-			className="h-full object-cover"
-		></video>
-	);
+  return (
+    <video
+      muted={currentStream}
+      autoPlay
+      ref={videoRef}
+      className="h-full object-cover"
+    ></video>
+  );
 }
 
 export default MediaItem;
