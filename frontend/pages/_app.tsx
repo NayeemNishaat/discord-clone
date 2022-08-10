660441b01072775d375672518dfa4b5f15239a87
nayeemnishaat <rxnayeemimi@gmail.com>
2022-08-11 00:57:56 +0600

    head updated

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,13 +5,13 @@ import store from "../redux/store";
 import Layout from "../components/Layout/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<Provider store={store}>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
 }
 
 export default MyApp;
